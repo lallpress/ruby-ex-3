faa1c1913b6cf4cc354f0cceb1e401101a2f86c1
Lauren Allpress <lallpress@unomaha.edu>
2017-06-04 18:51:39 +0000

    Recursive rock paper scissors solution and un-pending tests.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,29 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    unless ['R', 'P', 'S'].include? player2[1] 
+      raise NoSuchStrategyError,  "Strategy must be one of R,P,S"
+    end
+    
+    case player1[1]
+    when 'R'
+      ((player2[1] == 'R') || (player2[1] == 'S')) ? player1 : player2
+    when 'P'
+      ((player2[1] == 'P') || (player2[1] == 'R')) ? player1 : player2
+    when 'S'
+      ((player2[1] == 'S') || (player2[1] == 'P')) ? player1 : player2
+    else
+      raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament[0][0].is_a? String
+      winner(tournament[0], tournament[1])
+    else
+      tournament_winner(
+        [tournament_winner(tournament[0]), tournament_winner(tournament[1])]
+        )
+    end
   end
-
 end
