ef835e63e82a91257283117903833735138b954b
Lauren Allpress <lallpress@unomaha.edu>
2017-06-04 03:11:23 +0000

    Committing this file for now. Still not sure why tests are failing but giving no reasons?

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -5,10 +5,23 @@ module FunWithStrings
     self == self.reverse ? true : false
   end
   def count_words
-    # your code here
-  end
+    self.downcase!
+    words = self.split
+    word_count = {}
+    words.map!{ |word| word.gsub(/\W/,"") }
+    words.reject!{|word| word == ""}
+    words.each {|word| word_count.key?(word) ? (word_count[word] +=1) : (word_count[word] = 1) }
+    return word_count
+  end  
   def anagram_groups
-    # your code here
+    words = self.split
+    groups = []
+    words.each do |word|
+      sub_array = words.select { |second_word| word.downcase.chars.sort == second_word.downcase.chars.sort }
+      words = words - sub_array
+      groups.push(sub_array)
+    end
+    return groups
   end
 end
 
