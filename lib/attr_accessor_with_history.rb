8abe28fa8a4699a3e4c41800747ab8640d868799
Lauren Allpress <lallpress@unomaha.edu>
2017-06-06 04:00:15 +0000

    attribute accessor with history and associated spec file

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,22 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}= attr_value
+      
+        # if attr_name has no history...
+        # create an attr_name history in an empty array
+        if @#{attr_name}_history.nil?
+          @#{attr_name}_history = []
+        end
+        
+        # push the attr_name onto its history array
+        @#{attr_name}_history.push(@#{attr_name})
+      
+        # give attr name a value
+        @#{attr_name} = attr_value
+        
+        return @#{attr_name}_history
+      end
     }
   end
 end
